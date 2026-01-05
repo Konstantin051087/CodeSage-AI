f5ce146f6a7ddb070d9749ddce7d44b009da65d9
Konstantin051087 <44120633+Konstantin051087@users.noreply.github.com>
2026-01-05 22:54:34 +0500

    Update file

--- tests/test_sql_detector.py
+++ tests/test_sql_detector.py
@@ -1,7 +1,6 @@
 import libcst as cst
 import textwrap
 from core.detectors import SQLInjectionDetector
-from core.detectors import analyze_code
 from core.detectors import DangerousFunctionDetector
 
 def test_fstring_vulnerability():
@@ -23,8 +22,3 @@ def test_pickle_loads():
     wrapper = cst.MetadataWrapper(module)
     wrapper.visit(detector)
     assert len(detector.vulnerabilities) == 1
-
-def test_safe_query():
-    code = 'cursor.execute("SELECT * FROM users WHERE name = %s", (name,))'
-    # Должен вернуть 0 уязвимостей
-    assert len(analyze_code(code)) == 0
